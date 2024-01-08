790d61efdab12c691ba1bf2bf385818dc1a0956c
mohammed-khachachi <khachachivscod@gmail.com>
2024-01-08 08:54:31 -0800

    i add task 8!

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 =word[:3]
+word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = wor([1:-1]
 print(f"First 3 letters: {}".format(word_first_3))
